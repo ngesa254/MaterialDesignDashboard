7995f4bc4e6e61231a02bdb7d86a5cacd7ade1b0
ngesa marvin <marvinngesa@gmail.com>
2018-01-26 05:36:24 +0300

    material design dash board initial commit 📟

---
+++ app/src/main/java/com/droidmarvin/materialdesigndashboard/HomeDashboardActivity.java
@@ -0,0 +1,13 @@
+package com.droidmarvin.materialdesigndashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_dashboard);
+    }
+}
